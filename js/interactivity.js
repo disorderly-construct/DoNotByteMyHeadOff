52e7660df159eeed929aae34f6a75e7d97b63e72
disorderly-construct <117865654+disorderly-construct@users.noreply.github.com>
2023-05-29 19:37:59 -0400

    functional stars, twinkle and random sizes

---
+++ js/interactivity.js
@@ -0,0 +1,13 @@
+const stars = document.getElementById('stars');
+
+for (var i = 0; i < 750; i++) {
+    var star = document.createElement('div');
+    star.className = 'star';
+    star.style.top = Math.random() * window.innerHeight + 'px';
+    star.style.left = Math.random() * window.innerWidth + 'px';
+    star.style.animationDelay = Math.random() * 5 + 's';
+    var size = Math.random() * 2 + 'px';
+    star.style.width = size;
+    star.style.height = size;
+    stars.appendChild(star);
+}
